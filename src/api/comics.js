66a681482812d70a0917103b22db886463071ec2
zzb-666-q <1278369564@qq.com>
2024-09-11 14:00:26 +0800

    first commit

--- src/api/comics.js
+++ src/api/comics.js
@@ -1,27 +1,27 @@
-import request from '@/utils/request'
+import request from '@/utils/request';
 
 //漫画搜索
 export function comicSearch(query) {
   return request({
     url: '/Search',
     method: 'get',
-    params: query
-  })
+    params: query,
+  });
 }
 //漫画搜索
 export function ClassPage(query) {
   return request({
     url: 'ClassPage',
     method: 'get',
-    params: query
-  })
+    params: query,
+  });
 }
 
 // 新增承运商揽件地址绑定
 export function addBinding(data) {
-    return request({
-      url: '/carrier/binding',
-      method: 'post',
-      data: data
-    })
-  }
+  return request({
+    url: '/carrier/binding',
+    method: 'post',
+    data: data,
+  });
+}
--- vue.config.js
+++ vue.config.js
@@ -2,39 +2,20 @@ const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
 
 module.exports = defineConfig({
-        transpileDependencies: true,
-        publicPath: './',
-        // devServer: {
-        //     host: 'localhost',
-        //     port: 8080, // 端口号
-        //     open: true, //运行后是否自动在浏览器打开
-        // },
-        pluginOptions: {
-            "style-resources-loader": {
-                preProcessor: "less",
-                patterns: [
-                    // 全局变量路径
-                    path.resolve(__dirname, "./src/assets/css/global.less"),
-                ],
-            },
-        },
-
-    })
-    // module.exports = {
-    //     // devServer: {
-    //     //   host: 'localhost',
-    //     //   port: 8080, // 端口号
-    //     //   open:true, //运行后是否自动在浏览器打开
-    //     // },
-    //     pluginOptions: {
-    //         "style-resources-loader": {
-    //             preProcessor: "less",
-    //             patterns: [
-    //                 // 全局变量路径
-    //                 path.resolve(__dirname, "./src/assets/global.less"),
-    //             ],
-    //         },
-    //     },
-
-
-// }
+  transpileDependencies: true,
+  publicPath: './',
+  // devServer: {
+  //     host: 'localhost',
+  //     port: 8080, // 端口号
+  //     open: true, //运行后是否自动在浏览器打开
+  // },
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+        // 全局变量路径
+        path.resolve(__dirname, './src/assets/css/global.less'),
+      ],
+    },
+  },
+});
